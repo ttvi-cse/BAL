cce7744a2013903abd01af96b2a5531de34ba242
Tran Trung Vi <vi.tt@sutrixsolutions.com>
2017-06-08 18:06:02 +0700

    bubble sort

---
+++ algs/sort/Bubble.java
@@ -0,0 +1,19 @@
+import java.util.Arrays;
+class Bubble {
+    static void sort(int[] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr.length - i - 1; j++) {
+                if (arr[j] > arr[j+1]) {
+                    int t = arr[j];
+                    arr[j] = arr[j+1];
+                    arr[j+1] = t;
+                }
+            }
+        }
+    }
+    public static void main(String[] args) {
+        int[] arr = {64, 34, 25, 12, 22, 11, 90};
+        sort(arr);
+        System.out.println(Arrays.toString(arr));
+    }
+}
